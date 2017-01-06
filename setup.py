3c772cb6edebe221ec9bdcd1474ed76fb64fbef0
Adam Miller <maxamillion@fedoraproject.org>
2017-01-06 14:38:01 -0600

    Release 0.0.4
    Signed-off-by: Adam Miller <maxamillion@fedoraproject.org>

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ description = "Event loop driven Ansible playbook execution engine"
 
 setup(
     name='loopabull',
-    version='0.0.3',
+    version='0.0.4',
     description=description,
     long_description=description,
     classifiers=[
